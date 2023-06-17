2c6f2307249e029a90d23d608cbb0341cac1ecd9
simon0820s <simon0820s@gmail.com>
2023-06-17 12:42:28 -0500

    hello world

---
+++ helloWorld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+contract HolaMundo {
+
+    function sayHello() public pure returns (string memory){
+        return "hola mundo";
+    }
+}
